67c6340218ddbed1389d54bcf156c48f543975b7
evandenmark <evanlewisdenmark@gmail.com>
2023-10-27 14:01:25 -0400

    reput the shorts and slam onn there

--- src/homepage.tsx
+++ src/homepage.tsx
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate();
     return(
         <div className="home">
             <h1>This is the Home page</h1>
+            <button onClick={() => navigate('/futureancestors')}>Future Ancestors</button>
         </div>
     )
 }
